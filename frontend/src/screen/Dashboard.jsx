c062a7225901175b33705d554bc0a7a974ffbb30
Nitin Yadav <77772979+Nitin-kumar-yadav@users.noreply.github.com>
2025-11-22 22:58:00 +0530

    Fix optional chaining for weather data and add event section to Dashboard

--- frontend/src/screen/Dashboard.jsx
+++ frontend/src/screen/Dashboard.jsx
@@ -106,16 +106,31 @@ function Dashboard() {
                     </div>
                     <div className="flex flex-row justify-between aligin-center text-center">
                         <h1 className="text-5xl text-center aligin-center font-bold pt-2 text-transparent bg-clip-text 
-               bg-gradient-to-r from-indigo-500 to-purple-400 ">{Math.round(weatherData.main.temp - 273.15)} C</h1>
+               bg-gradient-to-r from-indigo-500 to-purple-400 ">{Math.round(weatherData?.main?.temp - 273.15)} C</h1>
                         <div className="">
                             <ThermometerSun className="w-10 h-10 text-indigo-600 dark:text-indigo-400" />
                         </div>
                     </div>
                     <div className="">
-                        <h1 className="text-base text-gray-500 font-medium">{weatherData.name}, {weatherData.sys.country}</h1>
+                        <h1 className="text-base text-gray-500 font-medium">{weatherData?.name}, {weatherData?.sys?.country}</h1>
                     </div>
                 </div>
 
+            </div>
+            <div className="pt-5">
+                <div className="flex flex-row justify-between max-w-8xl mx-5 h-[100px] px-5 bg-white/30 dark:bg-black/20 
+                        backdrop-blur-md 
+                        border-b border-white/20 dark:border-white/10
+                        shadow-lg transition-all duration-300 rounded-xl">
+                    <div className="flex flex-col justify-center gap-2 align-center ">
+                        <h1 className='text-2xl text-transparent bg-clip-text 
+               bg-gradient-to-r from-indigo-500 to-purple-400 font-extrabold'>Add Your Events</h1>
+                        <p className='text-base text-gray-500 font-medium'>0 event scheduled</p>
+                    </div>
+                    <div className="flex justify-center align-center text-center pt-5">
+                        <button className='bg-gray-200 dark:bg-gray-700 rounded-lg text-xl font-medium w-[150px] h-[45px] text-purple-400'>Add Event</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
