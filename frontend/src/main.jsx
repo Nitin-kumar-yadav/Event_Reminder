40fd2b1c9d4f345d588a15add95c00f273196b57
Nitin Yadav <77772979+Nitin-kumar-yadav@users.noreply.github.com>
2025-11-24 18:32:25 +0530

    feat: Initialize project structure with React, Express, and environment variable configuration for frontend and backend.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-export const BasicUrl = 'http://localhost:5000'
+export const BasicUrl = import.meta.env.VITE_BACKEND_URL
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
